6daeb61750a0b35fffaae0462fc7be79c3165c0e
Romerio Ribeiro <romerio111@hotmail.com>
2021-06-26 17:27:01 -0300

    final nlw 06

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import { ButtonHTMLAttributes } from 'react';
+import cx from 'classnames';
+
+import './styles.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
+
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
+  return (
+    <button className={cx('button', { outlined: isOutlined })} {...props} />
+  );
+}
